6008811fd71f5eb4c7ca563a4fb3f3c23cc2c087
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-07-02 22:47:30 -0300

    Londrina:02.07.22_22:48hs

--- app/Http/Controllers/ApiDash/HomeController.php
+++ app/Http/Controllers/ApiDash/HomeController.php
@@ -2,12 +2,50 @@
 
 namespace App\Http\Controllers\ApiDash;
 
-use App\Http\Controllers\Controller;
+use App\Models\Client;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
-class HomeController extends Controller {
-    public function index() {
-        return response()->json( [ 'status' => true ]);
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $clients = Client::all();
+
+        /* $clients = [
+            [
+                'id' => 1,
+                'name' => 'José Ricardo',
+                'email' => 'jose@gmail.com'
+            ],
+            [
+                'id' => 2,
+                'name' => 'José RMarcello',
+                'email' => 'joseMarcello@gmail.com'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Gabriel Silva Marcello',
+                'email' => 'gabrielsMarcello@gmail.com'
+            ]
+        ]; */
+
+        $products = [
+            [
+                'id' => 1,
+                'name' => 'Video game',
+                'value' => '10.50'
+            ],
+            [
+                'id' => 2,
+                'name' => 'X Box',
+                'value' => '44.50'
+            ]
+        ];
+
+        return response()->json([
+            'clienttts' => $clients,
+            'producttts' => $products
+        ]);
     }
 }
-
